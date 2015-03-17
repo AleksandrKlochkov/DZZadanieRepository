efb19fc8e7e65e53652798c7d9cfc5e91b5991c9
AleksandrKlochkov <surfer.ru@mail.ru>
2015-03-17 23:48:43 +0300

    #1
    :)

---
+++ Тестовое задание на С# Клочкова Александра/Исходные файлы приложения/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитеФайлСТекстомToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитеФайлСловаряToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.записатьФайлыВHTMLдокументToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьWebстраницуВБраузереToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.просмотрСправкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияОПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(203, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1.Загрузите файл с текстом";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(203, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2.Загрузите файл словаря";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(203, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(253, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3.Записать файлы в HTML-документ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(203, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(253, 56);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4.Открыть Web-страницу в браузере";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(386, 338);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 37);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Выход";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(629, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитеФайлСТекстомToolStripMenuItem,
+            this.загрузитеФайлСловаряToolStripMenuItem,
+            this.записатьФайлыВHTMLдокументToolStripMenuItem,
+            this.открытьWebстраницуВБраузереToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.просмотрСправкиToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.информацияОПрограммеToolStripMenuItem});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // загрузитеФайлСТекстомToolStripMenuItem
+            // 
+            this.загрузитеФайлСТекстомToolStripMenuItem.Name = "загрузитеФайлСТекстомToolStripMenuItem";
+            this.загрузитеФайлСТекстомToolStripMenuItem.Size = new System.Drawing.Size(276, 22);
+            this.загрузитеФайлСТекстомToolStripMenuItem.Text = "1,Загрузите файл с текстом";
+            this.загрузитеФайлСТекстомToolStripMenuItem.Click += new System.EventHandler(this.загрузитеФайлСТекстомToolStripMenuItem_Click);
+            // 
+            // загрузитеФайлСловаряToolStripMenuItem
+            // 
+            this.загрузитеФайлСловаряToolStripMenuItem.Name = "загрузитеФайлСловаряToolStripMenuItem";
+            this.загрузитеФайлСловаряToolStripMenuItem.Size = new System.Drawing.Size(276, 22);
+            this.загрузитеФайлСловаряToolStripMenuItem.Text = "2.Загрузите файл словаря";
+            this.загрузитеФайлСловаряToolStripMenuItem.Click += new System.EventHandler(this.загрузитеФайлСловаряToolStripMenuItem_Click);
+            // 
+            // записатьФайлыВHTMLдокументToolStripMenuItem
+            // 
+            this.записатьФайлыВHTMLдокументToolStripMenuItem.Name = "записатьФайлыВHTMLдокументToolStripMenuItem";
+            this.записатьФайлыВHTMLдокументToolStripMenuItem.Size = new System.Drawing.Size(276, 22);
+            this.записатьФайлыВHTMLдокументToolStripMenuItem.Text = "3.Записать файлы в HTML-документ";
+            this.записатьФайлыВHTMLдокументToolStripMenuItem.Click += new System.EventHandler(this.записатьФайлыВHTMLдокументToolStripMenuItem_Click);
+            // 
+            // открытьWebстраницуВБраузереToolStripMenuItem
+            // 
+            this.открытьWebстраницуВБраузереToolStripMenuItem.Name = "открытьWebстраницуВБраузереToolStripMenuItem";
+            this.открытьWebстраницуВБраузереToolStripMenuItem.Size = new System.Drawing.Size(276, 22);
+            this.открытьWebстраницуВБраузереToolStripMenuItem.Text = "4.Открыть Web-страницу в браузере";
+            this.открытьWebстраницуВБраузереToolStripMenuItem.Click += new System.EventHandler(this.открытьWebстраницуВБраузереToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(276, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // просмотрСправкиToolStripMenuItem
+            // 
+            this.просмотрСправкиToolStripMenuItem.Name = "просмотрСправкиToolStripMenuItem";
+            this.просмотрСправкиToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.просмотрСправкиToolStripMenuItem.Text = "Просмотр справки";
+            this.просмотрСправкиToolStripMenuItem.Click += new System.EventHandler(this.просмотрСправкиToolStripMenuItem_Click);
+            // 
+            // информацияОПрограммеToolStripMenuItem
+            // 
+            this.информацияОПрограммеToolStripMenuItem.Name = "информацияОПрограммеToolStripMenuItem";
+            this.информацияОПрограммеToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.информацияОПрограммеToolStripMenuItem.Text = "Информация о программе";
+            this.информацияОПрограммеToolStripMenuItem.Click += new System.EventHandler(this.информацияОПрограммеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 422);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Тестовое задание Клочкова Александра Евгеньевича";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитеФайлСТекстомToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитеФайлСловаряToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem записатьФайлыВHTMLдокументToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьWebстраницуВБраузереToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem просмотрСправкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияОПрограммеToolStripMenuItem;
+    }
+}
+
---
+++ Тестовое задание на С# Клочкова Александра/Исходные файлы приложения/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public static string a, b, c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myFile = new OpenFileDialog();
+
+            myFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";// Установка ограничения по выбору расширения файла
+            if (myFile.ShowDialog() == DialogResult.OK)// Проверка загрузки файла
+            {
+                a = myFile.FileName;// Присваивание переменной значения выбранного файла
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myFile = new OpenFileDialog();
+            myFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";// Установка ограничения по выбору расширения файла
+            if (myFile.ShowDialog() == DialogResult.OK)// Проверка загрузки файла
+            {
+                b = myFile.FileName;// Присваивание переменной значения выбранного файла
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if ((a == null) && (b == null))
+            {
+                MessageBox.Show("Выберите файлы для загрузки. (Выполните пункты 1-2)");
+            }
+            else
+            {
+                if (a == null)
+                {
+                    MessageBox.Show("Файл с текстом не загружен. (Выполните пункт 1)");
+                }
+                else
+                {
+                    if (b == null)
+                    {
+                        MessageBox.Show("Словарь не загружен. (Выполните пункт 2)");
+                    }
+                    else
+                    {
+                        string[] s2 = File.ReadAllLines(b, Encoding.GetEncoding("windows-1251")); // Создание массива и считывание содержимого файла  
+                        FileStream file1 = new FileStream("d:\\myprogramma.html", FileMode.Create); //Создание нового файла
+                        c = "d:\\myprogramma.html";
+                        StreamWriter writer = new StreamWriter(file1, Encoding.GetEncoding("windows-1251"));// Соединение файлового потока с "Потоковым писателем"          
+                        FileInfo file = new FileInfo(a);
+                        long size = file.Length;
+                        writer.Write("<!DOCTYPE HTML>");
+                        writer.Write("<html><head>");
+                        writer.Write("<title>Тестовое задание Клочкова Александра</title>");
+                        writer.Write("</head>");
+                        writer.Write("<body>");
+                        writer.Write("<div><p>");
+                        if (size > 2100000)
+                        {
+                            MessageBox.Show("Возникла ошибка при загрузке файла. Файл превышает допустимый размер (2 Мб)");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Загрузка файла произведена успешно.");
+                            string[] OurText = File.ReadAllLines(a, Encoding.GetEncoding("windows-1251")); // Создание массива и считывание содержимого файла
+                            List<string> EndText = new List<string>();
+                            bool flag = false;
+                            foreach (string str in OurText)   //Просмотр одна за одной всех строк в тексте
+                            {
+                                string[] slovo = str.Split(' '); // Разбиение строки на слова и запись в массив
+                                for (int i = 0; i < slovo.Length; i++) // Организация цикла, обрабатывающего содержимое массива до конца файла
+                                {
+                                    flag = false;
+                                    for (int j = 0; j < s2.Length; j++)// Организация цикла для проверки условия равенства слов
+                                    {
+                                        if (slovo[i] == s2[j])// Проверка слов. Если слова равны, то
+                                        {
+                                            writer.Write("<strong><em>" + slovo[i] + "</em></strong>" + " ");//Замена слова из файла с текстом на слово из словаря
+                                            flag = true;
+                                        }
+                                    }
+                                    if (flag != true)
+                                    {
+                                        writer.Write(slovo[i] + " "); // Если слово из файла с текстом не равно слову из словаря, то замены не происходит
+                                    }
+
+                                }
+                                writer.Write("<br>");// Переход на следующую строку в файле с текстом
+                            }
+
+
+                        }
+                        writer.Write("<p></div>");
+                        writer.Write("</body>");
+                        writer.Write("</html>");
+
+
+                        writer.Close();// Закрытие файлового потока
+                    }
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (c == "d:\\myprogramma.html") // Проверка наличия файла
+            {
+                System.Diagnostics.Process.Start("d:\\myprogramma.html");//Если файл существует - открытие его в браузере 
+            }
+            else
+            {
+                MessageBox.Show("Файл, который Вы пытаетесь загрузить, не найден. Убедитесь в его существовании.");// Если файл отсутствует - соответствуеющее сообщение 
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void загрузитеФайлСТекстомToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myFile = new OpenFileDialog();
+
+            myFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";// Установка ограничения по выбору расширения файла
+            if (myFile.ShowDialog() == DialogResult.OK)// Проверка загрузки файла
+            {
+                a = myFile.FileName;// Присваивание переменной значения выбранного файла
+            }
+        }
+
+        private void загрузитеФайлСловаряToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myFile = new OpenFileDialog();
+            myFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";// Установка ограничения по выбору расширения файла
+            if (myFile.ShowDialog() == DialogResult.OK)// Проверка загрузки файла
+            {
+                b = myFile.FileName;// Присваивание переменной значения выбранного файла
+            }
+        }
+
+        private void записатьФайлыВHTMLдокументToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((a == null) && (b == null))
+            {
+                MessageBox.Show("Выберите файлы для загрузки. (Выполните пункты 1-2)");
+            }
+            else
+            {
+                if (a == null)
+                {
+                    MessageBox.Show("Файл с текстом не загружен. (Выполните пункт 1)");
+                }
+                else
+                {
+                    if (b == null)
+                    {
+                        MessageBox.Show("Словарь не загружен. (Выполните пункт 2)");
+                    }
+                    else
+                    {
+                        string[] s2 = File.ReadAllLines(b, Encoding.GetEncoding("windows-1251")); // Создание массива и считывание содержимого файла  
+                        FileStream file1 = new FileStream("d:\\myprogramma.html", FileMode.Create); //Создание нового файла
+                        c = "d:\\myprogramma.html";
+                        StreamWriter writer = new StreamWriter(file1, Encoding.GetEncoding("windows-1251"));// Соединение файлового потока с "Потоковым писателем"          
+                        FileInfo file = new FileInfo(a);
+                        long size = file.Length;
+                        writer.Write("<!DOCTYPE HTML>");
+                        writer.Write("<html><head>");
+                        writer.Write("<title>Тестовое задание Клочкова Александра</title>");
+                        writer.Write("</head>");
+                        writer.Write("<body>");
+                        writer.Write("<div><p>");
+                        if (size > 2100000)
+                        {
+                            MessageBox.Show("Возникла ошибка при загрузке файла. Файл превышает допустимый размер (2 Мб)");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Загрузка файла произведена успешно.");
+                            string[] OurText = File.ReadAllLines(a, Encoding.GetEncoding("windows-1251")); // Создание массива и считывание содержимого файла
+                            List<string> EndText = new List<string>();
+                            bool flag = false;
+                            foreach (string str in OurText)   //Просмотр одна за одной всех строк в тексте
+                            {
+                                string[] slovo = str.Split(' '); // Разбиение строки на слова и запись в массив
+                                for (int i = 0; i < slovo.Length; i++) // Организация цикла, обрабатывающего содержимое массива до конца файла
+                                {
+                                    flag = false;
+                                    for (int j = 0; j < s2.Length; j++)// Организация цикла для проверки условия равенства слов
+                                    {
+                                        if (slovo[i] == s2[j])// Проверка слов. Если слова равны, то
+                                        {
+                                            writer.Write("<strong><em>" + slovo[i] + "</em></strong>" + " ");//Замена слова из файла с текстом на слово из словаря
+                                            flag = true;
+                                        }
+                                    }
+                                    if (flag != true)
+                                    {
+                                        writer.Write(slovo[i] + " "); // Если слово из файла с текстом не равно слову из словаря, то замены не происходит
+                                    }
+
+                                }
+                                writer.Write("<br>");// Переход на следующую строку в файле с текстом
+                            }
+
+
+                        }
+                        writer.Write("<p></div>");
+                        writer.Write("</body>");
+                        writer.Write("</html>");
+
+
+                        writer.Close();// Закрытие файлового потока
+                    }
+                }
+            }
+        }
+
+        private void открытьWebстраницуВБраузереToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (c == "d:\\myprogramma.html") // Проверка наличия файла
+            {
+                System.Diagnostics.Process.Start("d:\\myprogramma.html");//Если файл существует - открытие его в браузере 
+            }
+            else
+            {
+                MessageBox.Show("Файл, который Вы пытаетесь загрузить, не найден. Убедитесь в его существовании.");// Если файл отсутствует - соответствуеющее сообщение 
+            }
+        }
+
+        private void просмотрСправкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа очень проста в использовании!Для ее правильной, вам нужно для начала нажать клавишу (1.Загрузить файл с текстом) и выбрать нужный вам файл с текстом.=>>>Потом нажать клавишу для загрузки файла словаря (2.Загрузить файл словаря)и выбрать файл (Slovar.txt).=>>>Далее нажать клавишу (3.Записать файлы в HTML-документ).=>>>И затем нажмите (4.Открыть HTML-документ в браузере).Если вы все сделали правильно приложение выдаст вам нужную html-страницу. Аналогичные действия можно проделать через контекстное меню программы.Клавиша (Выход) закроет приложение.Также можно найти в контекстном меню информацию о приложении.<<<=Спасибо за внимание!!!=>>>  ", "Help");
+        }
+
+        private void информацияОПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа была выполненна Клочковым Александром, с целью выполнения тестового задания", "info");
+        }
+
+    }
+}
